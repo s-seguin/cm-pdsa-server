e025c39f6e1f0373f392fe73e039f5b9a1715899
Stuart Seguin <sseguin@criticalmass.com>
2019-06-13 13:49:24 -0600

    [feature/docs] changed functions to arrow functions

--- src/controllers/kittenController.js
+++ src/controllers/kittenController.js
@@ -5,43 +5,43 @@ import Kitten from '../database/models/kitten';
  * @param {*} req the http request object
  * @param {*} res http response object
  */
-export function findByName(req, res) {
+export const findByName = (req, res) => {
   Kitten.find({ name: req.params.name }, (err, kittens) => {
     if (err) return console.error(err);
     return res.json(kittens);
   });
-}
+};
 
 /**
  * Get all of the kittens that match the age in the request, and send them in the response as JSON.
  * @param {*} req the http request object
  * @param {*} res the http response object
  */
-export function findByAge(req, res) {
+export const findByAge = (req, res) => {
   Kitten.find({ age: req.params.age }, (err, kittens) => {
     if (err) return console.error(err);
     return res.send(kittens);
   });
-}
+};
 
 /**
  * Get all of the kittens, and send them in the response as JSON.
  * @param {*} req the http request object
  * @param {*} res the http response object
  */
-export function findAll(req, res) {
+export const findAll = (req, res) => {
   Kitten.find((err, kittens) => {
     if (err) return console.error(err);
     return res.send(kittens);
   });
-}
+};
 
 /**
  * Create a new kitten with the name and age parameters in the request, send result back in response.
  * @param {*} req the http request object
  * @param {*} res the http response object
  */
-export function newKitten(req, res) {
+export const newKitten = (req, res) => {
   const kitty = new Kitten({
     name: req.params.name,
     age: req.params.age
@@ -52,15 +52,15 @@ export function newKitten(req, res) {
       res.send(err);
     } else res.sendStatus(200);
   });
-}
+};
 
 /**
  * Rename the kitten specified in the request.
  * @param {*} req the http request object
  * @param {*} res the http response object
  */
-export function rename(req, res) {
+export const rename = (req, res) => {
   const result = Kitten.updateMany({ name: req.params.oldName }, { name: req.params.newName });
   console.log(result);
   res.send(result.n);
-}
+};
